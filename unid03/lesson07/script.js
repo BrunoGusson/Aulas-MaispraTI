c55278f6c7ed4711eb3a6dd7729d07b8c42288b3
Bruno Gusson <brunogusson20@gmail.com>
2024-07-12 21:04:15 -0300

    Continuação aulas

---
+++ unid03/lesson07/script.js
@@ -0,0 +1,19 @@
+// document.getElementById('nome').value = 'Santos'
+ // let nome = prompt('Qual é o seu nome?')
+        // let idade = parseInt(prompt('Qual é sua idade?'))
+        // let idade2 = parseInt(prompt('Digite outra idade?'))
+
+        // let media = (idade + idade2) / 2;
+
+        // document.write(`<h1>Olá, seu nome é ${nome} e sua idade é ${idade} e a média das idades é ${media} </h1>`)
+// function eventoMouse(){
+//     document.write('Evento aconteceu!')
+// }
+
+// function eventoTeclado() {
+//     document.write('Evento aconteceu!')
+// }
+
+function eventoJanela() {
+    document.write('Evento aconteceu!')
+}
